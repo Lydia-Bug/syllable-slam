7953296d82b5009cbec80887d6679494dd1c0f81
broni673 <broni673@student.otago.ac.nz>
2023-03-07 15:52:44 +1300

    push

--- Syllables.java
+++ Syllables.java
@@ -21,19 +21,19 @@ public class Syllables{
         ArrayList<String> blends = new ArrayList<String>(Arrays.asList("bl", "br", "cl", "cr", "dr", "fr", "tr", "fl", "gl", "gr", "pl", "pr", "sl", "sm", "sp", "st"));
 
         for(int i = 0; i < word.length(); i++){
-            if(vowels.contains(Sub(word, i))){
-                if(Sub(word, i+1).equals("r")){
+            if(vowels.contains(Sub(word, i))){ // if the current letter is a vowel
+                if(Sub(word, i+1).equals("r")){ // if the next letter is "r" eg. "ar", "er"
                     syllables++;
-                } else if(Sub(word, i).equals("u")){
-                    if(Sub(word, i+1).equals("a") && !Sub(word, i-1).equals("q")){
+                } else if(Sub(word, i).equals("u")){ // If the current letter is "u"
+                    if(Sub(word, i+1).equals("a") && !Sub(word, i-1).equals("q")){ // if the next letter is "a" and the previous letter is NOT "q"
                         syllables++;
-                    } else if(Sub(word, i + 1).equals("o") && Sub(word,i+2).equals("u")){
+                    } else if(Sub(word, i + 1).equals("o") && Sub(word,i+2).equals("u")){ // if the next two letters are "uo"
                         syllables++;
                     }
                 }
             }
             if(i == word.length() - 3){
-                if(consonants.contains(Sub(word, i)) && Sub(word, i+1).equals("l") && Sub(word,i+2).equals("e")){
+                if(consonants.contains(Sub(word, i)) && Sub(word, i+1).equals("l") && Sub(word,i+2).equals("e")){ // if the word ends in a consonant and then "le"
                     syllables++;
                 }
             }
