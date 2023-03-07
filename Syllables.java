eb55b9f6b697bcc0acccd45436d298177fbd71c9
broni673 <broni673@student.otago.ac.nz>
2023-03-07 15:43:35 +1300

    yo

--- Syllables.java
+++ Syllables.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Syllables{
     public static void main(String[] args){
@@ -13,14 +15,27 @@ public class Syllables{
         word = word.toLowerCase();
         String[] wordArr = word.split("");
         int syllables = 0;
+        ArrayList<String> vowels = new ArrayList<String>(Arrays.asList("a", "e", "i", "o", "u"));
+        ArrayList<String> vowls = new ArrayList<String>(Arrays.asList("a", "i", "o", "u"));
+        ArrayList<String> consonants = new ArrayList<String>(Arrays.asList("b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"));
+        ArrayList<String> blends = new ArrayList<String>(Arrays.asList("bl", "br", "cl", "cr", "dr", "fr", "tr", "fl", "gl", "gr", "pl", "pr", "sl", "sm", "sp", "st"));
 
-        if(word.length() > 0){
-            syllables++;
-        }
-        
         for(int i = 0; i < word.length(); i++){
-            if(Sub(word, i).equals("u") && Sub(word, i+1).equals("a") && !Sub(word, i-1).equals("q")){
-                syllables++;
+            if(vowels.contains(Sub(word, i))){
+                if(Sub(word, i+1).equals("r")){
+                    syllables++;
+                } else if(Sub(word, i).equals("u")){
+                    if(Sub(word, i+1).equals("a") && !Sub(word, i-1).equals("q")){
+                        syllables++;
+                    } else if(Sub(word, i + 1).equals("o") && Sub(word,i+2).equals("u")){
+                        syllables++;
+                    }
+                }
+            }
+            if(i == word.length() - 3){
+                if(consonants.contains(Sub(word, i)) && Sub(word, i+1).equals("l") && Sub(word,i+2).equals("e")){
+                    syllables++;
+                }
             }
         }
 
