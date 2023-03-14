5b94e8e02c6a1147f6fc6b84b0c3d0d49dd204e9
trinicorn <therealfuz98@gmail.com>
2023-03-14 14:12:29 +1300

    ES works witht the exception of the L rule

--- Syllables.java
+++ Syllables.java
@@ -22,10 +22,16 @@ public class Syllables{
         ArrayList<String> vowls = new ArrayList<String>(Arrays.asList("a", "i", "o", "u"));
         ArrayList<String> consonants = new ArrayList<String>(Arrays.asList("b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"));
         ArrayList<String> blends = new ArrayList<String>(Arrays.asList("bl", "br", "cl", "cr", "dr", "fr", "tr", "fl", "gl", "gr", "pl", "pr", "sl", "sm", "sp", "st"));
-        
+        ArrayList<String> esException = new ArrayList<String>(Arrays.asList("c","x","g"));
+
         //a group is a group of vowels and surrounding constantants; eg banana [ban, nan, na]
         //the base rule is that every group is one syllable
         ArrayList<String> groups = splitWordIntoGroups(word); 
+        //Saves last word group to variable
+        String lastGroup = groups.get(groups.size()-1);
+        //Splits last word group into individual character strings and puts them into lastWordGroup
+        String[] lastWordGroup = lastGroup.split("(?!^)");
+        
 
         for(int i = 0; i < groups.size(); i++){
             //'u|ou' e.g. continuous, superfluous, etc
@@ -39,6 +45,18 @@ public class Syllables{
             
         }
 
+    
+        if (lastWordGroup[lastWordGroup.length-1].equals("e")){
+            syllables --;
+        }else if (lastWordGroup[lastWordGroup.length-2].equals("e") &&  lastWordGroup[lastWordGroup.length-1].equals("s")){
+            if(!lastWordGroup[lastWordGroup.length-3].equals("g")&&!lastWordGroup[lastWordGroup.length-3].equals("x")&&!lastWordGroup[lastWordGroup.length-3].equals("c")){
+                syllables --;   
+            }
+        }
+        
+
+    
+        
         /* 
         for(int i = 0; i < word.length(); i++){
             if(vowels.contains(Sub(word, i))){ // if the current letter is a vowel
@@ -86,22 +104,6 @@ public class Syllables{
                 }
                 
             }
-            if(Sub(word, i).equals("y")){
-                boolean surroundingConstants = false;
-                if(i != 0){
-                    if(vowels.contains(Sub(word, i-1))){
-                        surroundingConstants = true;
-                    }
-                }
-                if(i != word.length()-1){
-                    if(vowels.contains(Sub(word, i+1))){
-                        surroundingConstants = true;
-                    }
-                }
-                if(!surroundingConstants){
-                    vowelIndex.add(new int[]{i, i});
-                }
-            }
         }
         ArrayList<String> groups = new ArrayList<String>();
         for(int i = 0; i < vowelIndex.size(); i++){
@@ -122,4 +124,6 @@ public class Syllables{
     public static String Sub(String word, int index){
         return word.substring(index, index+1);
     }
+
+
 }
