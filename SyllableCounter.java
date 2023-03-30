04aeffee8942269948626aa4ef3e98fb358ed37a
Toby Sugrue <fien.joaas@gmail.com>
2023-03-30 13:45:44 +1300

    slight refactoring, added comments

--- SyllableCounter.java
+++ SyllableCounter.java
@@ -1,87 +1,62 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * Class to read (presumably) english words from stdin, outputting a syllable
+ * count to stdout.
+ * 
+ */
 public class SyllableCounter {
-    public static void main(String[] args) throws FileNotFoundException{
-        /**
-         * generate arraylist of VowelSet
-         * NOTE FOR CSV REGEX: need to escape backslash; not sure how well this will work yet
-         * 
-         * while input:
-         *      split line into syllables based on vowel groups
-         *      have some kind of handling for y
-         *      extract vowels
-         *      find vowels in vowelset arraylist
-         *          foreach vowel group: misinterpret
-         *                  [special if statement for handling final syllable exceptions]
-         *                  add to count based on rule matching
-         *                  add DEFAULT_SYLLABLES (2) if vowel combo not found
-         *          output count to stdout
-         */
 
-        
-        ArrayList<VowelSet> vowelSets = generateVowelSets("data/vowelData.csv");
-        //File f = new File("data/wordList.txt");
+    static ArrayList<VowelSet> vowelSets = generateVowelSets("vowelData.csv");
+
+    public static void main(String[] args) throws FileNotFoundException{
+
 
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
-            System.out.println(calculateSyllables(sc.nextLine(), vowelSets));
+            System.out.println(calculateSyllables(sc.nextLine()));
         }
         sc.close();
-        /* 
-        Scanner sc = new Scanner(f);
-        while(sc.hasNextLine()){
-            String line = sc.nextLine();
-            System.err.println(line + ":");
-            String[] word = Word.getWordGroups(line);
-            for(String group : word){
-                System.out.print(group + "\t");
-            }
-            System.out.println();
-        }
-        sc.close();
-        */
     
     }
 
-    public static int calculateSyllables(String input, ArrayList<VowelSet> vowelSets){
+    /**
+     * Calculates the number of syllables for a given word.
+     * 
+     * @param input a single word, not including whitespaces
+     * 
+     * @return syllable count for the whole word
+     */
+    public static int calculateSyllables(String input){
         ArrayList<String> word = Word.splitWordIntoGroups(input.toLowerCase());
         int syllables = 0;
         for(int i = 0; i < word.size(); i++){
-            String vowelGroup = getVowelGroup(word.get(i));
-            boolean isVowelSet = false;
-            for(VowelSet vs : vowelSets){
-                if(vs.isVowelSet(vowelGroup)){
-                    syllables += vs.compareSyllable(word.get(i));
-                    isVowelSet = true;
-                }
-            }
-            if(i == word.size()-1 && word.get(i).matches("[a-z]*e") && syllables >= 1){
-                // trailing e so no syllable
-                isVowelSet = true;
-            }
-            if(!isVowelSet){
-                syllables++; // if not in our vowel sets its presumed to be one syllable
-            }
+            syllables += sylCount(word.get(i));
         }
         return syllables;
     }
 
-    public static String getVowelGroup(String cvcGroup){
-        ArrayList<String> vowels = new ArrayList<String>(Arrays.asList("a", "e", "o", "i", "u"));
-        String vowelGroup = "";
-            for(int i = 0; i < cvcGroup.length(); i++){
-                if(vowels.contains(cvcGroup.substring(i, i+1))){
-                    vowelGroup += cvcGroup.substring(i, i+1);
-                }
+    /**
+     * Gets the syllable count for a particular vowel group based on the
+     * vowelSets data field.
+     * 
+     * @param input A vowel group, including surrounding consonants
+     * 
+     * @return Syllable count for the group, or 1 if no match is found
+     */
+    public static int sylCount(String input){
+        String vowelGroup = Word.getVowelGroup(input);
+        for(VowelSet vs : vowelSets){
+            if(vs.isVowelSet(vowelGroup)){
+                return vs.compareSyllable(input);
             }
-        return vowelGroup;
+        }
+        return 1; //assumes 1 syllable if the vowel combo isn't in our vowel sets
     }
 
     /**
@@ -104,8 +79,10 @@ public class SyllableCounter {
 				line = reader.readLine();
 			}
 			reader.close();
+
 		} catch (IOException e) {
 			e.printStackTrace();
+
 		}
         return vowelSets;
     }
