89b7e80da8ba94c31757df936cefc2d6883848cd
Lydia Acton <acton.lydia@gmail.com>
2023-03-31 13:16:52 +1300

    Changed testing so account for ambiguous words with multiple pronunciations

--- WordListTest.java
+++ WordListTest.java
@@ -1,38 +1,54 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
 import java.util.ArrayList;
 
 public class WordListTest {
     static int wordsChecked = 0;
     static int correctWords = 0;
+    // ambiguous words are words with two different amount of syllables
+    static int ambiguousWordsChecked = 0;
+    static int ambiguousWordsCorrect = 0;
     static ArrayList<ArrayList<String>> incorrectWords = new ArrayList<ArrayList<String>>();
+    static ArrayList<String> ambiguousIncorrectWords = new ArrayList<String>();
 
     public static void main(String[] args){
         readWordFile("wordsAndSyllables.txt");
-        System.out.println("Words checked: " + wordsChecked);
-        System.out.println("Words correct: " + correctWords);
-        double percentage = 100*(correctWords+0.0)/wordsChecked ;
-        System.out.println("Percent of words correct: " + String.format("%.2f", percentage) + "%\n");
+
+        System.out.println("[word]-[calculated syllables] [actual amount of syllables]\n");
         for(int i = 1; i < incorrectWords.size(); i++){
             System.out.println("Words " + i + " syllable off: ");
             System.out.println(incorrectWords.get(i).toString() +"\n");
         }
+        if(ambiguousIncorrectWords.size() > 0){
+            System.out.println("Ambiguous words incorrect:");
+            System.out.println(ambiguousIncorrectWords.toString() +"\n");
+        }
+        System.out.println("Words checked: " + wordsChecked);
+        System.out.println("Words correct: " + correctWords);
+        System.out.println("Ambiguous words checked: " + ambiguousWordsChecked);
+        System.out.println("Ambiguous words correct: " + ambiguousWordsCorrect);
+        double percentage = 100*(correctWords+0.0)/wordsChecked ;
+        System.out.println("Percent of words correct: " + String.format("%.2f", percentage) + "%\n");
     }
     
+     /**
+     * Reads in the lines from the file, and then checks them
+     * 
+     * 
+     * @param fileName name of file with data of words and their amount of syllables
+     */
     public static void readWordFile(String fileName){
         try {
           File myObj = new File(fileName);
           Scanner myReader = new Scanner(myObj);
           while (myReader.hasNextLine()) {
-            String[] line = myReader.nextLine().split(" ");
-            if(line.length > 2){
-                for(int i = 1; i < line.length-1; i++){
-                    line[0] = line[0] + line[i];
-                }
+            String[] line = myReader.nextLine().split(",");
+            int[] syllables = new int[line.length - 1];
+            for(int i = 1; i < line.length; i++){
+                syllables[i-1] = Integer.parseInt(line[i]);
             }
-            checkWord(line[0], Integer.parseInt(line[line.length-1]));
+            checkWord(line[0], syllables);
           }
           myReader.close();
         } catch (FileNotFoundException e) {
@@ -41,17 +57,38 @@ public class WordListTest {
         }
     }
 
-    public static void checkWord(String word, int syllables){
-        ArrayList<VowelSet> vowelSets = SyllableCounter.generateVowelSets("data/vowelData.csv");
-        int calculatedSyllables = SyllableCounter.calculateSyllables(word, vowelSets);
-        if(calculatedSyllables == syllables){
-            correctWords++;
-        }else{
-            int syllablesOff = Math.abs(calculatedSyllables - syllables);
-            while(incorrectWords.size() < syllablesOff+1){
-                incorrectWords.add(new ArrayList<String>());
+    /**
+     * Runs our code to calculate amount of syllables and checks against actual amount of syllables
+     * 
+     * 
+     * @param word word to check the amount of syllables of
+     * 
+     * @param syllables amount of syllables in word, this can be an array of size 2 if it's an ambiguous word
+     */
+    public static void checkWord(String word, int[] syllables){
+        int calculatedSyllables = SyllableCounter.calculateSyllables(word);
+        boolean correctAmountOfSyllables = false;
+        for(int i = 0; i < syllables.length; i++){
+            if(calculatedSyllables == syllables[i]){
+                correctWords++;
+                correctAmountOfSyllables = true;
             }
-            incorrectWords.get(syllablesOff).add(word + " " + calculatedSyllables + " " + syllables);
+        }
+        if(syllables.length == 2){
+            ambiguousWordsChecked++;
+            if(correctAmountOfSyllables){
+                ambiguousWordsCorrect++;
+            }else{
+                ambiguousIncorrectWords.add(word + "-" + calculatedSyllables + " " + syllables[0] + " " + syllables[1]);
+            }
+        }else{
+            if(!correctAmountOfSyllables){
+                int syllablesOff = Math.abs(calculatedSyllables - syllables[0]);
+                while(incorrectWords.size() < syllablesOff+1){
+                    incorrectWords.add(new ArrayList<String>());
+                }
+                incorrectWords.get(syllablesOff).add(word + "-" + calculatedSyllables + " " + syllables[0]);
+            }
         }
         wordsChecked++;
     }
--- generateTestFile.py
+++ generateTestFile.py
@@ -1,41 +1,62 @@
 import requests
+import re
 import json
 
 def addWordAndAmountOfSyllables(word):
-    print(word.strip())
-    amountOfSyllables = getAmountOfSyllables(word)
-    if amountOfSyllables != False:
-        words_and_syllables.write(word.strip() + " " + str(amountOfSyllables) + "\n")
+   amountOfSyllables = getAmountOfSyllables(word)
+   if amountOfSyllables != False:
+      outputLine = word.strip()
+      for syllables in amountOfSyllables:
+         outputLine = outputLine + "," + str(syllables)
+      print(outputLine)
+      words_and_syllables.write(outputLine + "\n")
     
+def getWordPronunciation(word):
+   # merriam webster api url
+   url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key=4ad66567-38f9-4f67-a045-c01421e84bd2"
+   request = requests.get(url)
+   wordJson = request.json()
+
+   dictWord = wordJson[0]
+   
+   # the resulting json structure isn't the same for all words. Some words like cleaning doesn't have it's own definition 
+   # because it's just the present tense of clean, so it doesn't have a way to pronounce it
+   # So thats why these checks are here rather then just getting the pronunciation 
+   if "hwi" in dictWord:
+      dictWord = dictWord["hwi"]
+   else:
+      return False
+    
+   if "prs" in dictWord:
+      wordPronunciation = dictWord["prs"][0]["mw"]
+      wordInDictionary = dictWord["hw"].replace("*", "")
+      if wordInDictionary != word.strip():
+         return False
+   else:
+      return False
+   
+   return wordPronunciation
+
 def getAmountOfSyllables(word):
-    hasWordPronunciation = False
+   wordPronunciation = getWordPronunciation(word)
 
-    url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key=4ad66567-38f9-4f67-a045-c01421e84bd2"
-    request = requests.get(url)
-    wordJson = request.json()
+   if wordPronunciation == False:
+      return False
 
-    dictWord = wordJson[0]
-    
-    if "hwi" in dictWord:
-       dictWord = dictWord["hwi"]
-    else:
-       return False
-    
-    if "prs" in dictWord:
-       wordPronunciation = dictWord["prs"][0]["mw"]
-       wordInDictionary = dictWord["hw"].replace("*", "")
-       if wordInDictionary != word.strip():
-            return False
-    else:
-       return False
+   otherWordPronunciation = re.sub("\(.*\)", "", wordPronunciation) # If something is in brackets it may or may not be pronounced, so the word can have multiple pronunciations
 
-    wordSyllables = wordPronunciation.split("-")
-    return len(wordSyllables)
+   # hyphens are used to separate syllables
+   wordSyllables = wordPronunciation.split("-") 
+   otherWordSyllables = otherWordPronunciation.split("-")
 
-#words = open("Test/wordList.txt", "r")
-words = open("Test/wordList.txt", "r")
+   if len(wordSyllables) == len(otherWordSyllables):
+      return [len(wordSyllables)]
+ 
+   return [len(wordSyllables), len(otherWordSyllables)]
 
-words_and_syllables = open('Test/wordsAndSyllables.txt', 'a')
+words = open("wordList.txt", "r")
+
+words_and_syllables = open('wordsAndSyllables.txt', 'a')
 
 for word in words:
   addWordAndAmountOfSyllables(word)
